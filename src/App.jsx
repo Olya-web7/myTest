903abea01a5fab6184e9f576939b0c1d6791d303
Olya <ola.shy@yahoo.com>
2022-04-18 22:17:26 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
+import Messages from './Messages';
 import Table from './Table';
-// import User from './user';
 import { Users } from './Users';
 
 function App() {
   return (
     <div className="app">
       <Table data={Users}/>
+      <Messages />
     </div>
   );
 }
--- src/Table.jsx
+++ src/Table.jsx
@@ -5,15 +5,29 @@ function Table({ data }) {
   return (
     <table className='wrapper'>
       <h1 className='title'>Selected Files</h1>
-      <tbody className="list">
-        {data.map((item) => (
-          <tr key={item.id} className="listItem">
-            <td>{item.name}</td>
-            <td>{item.role}</td>
-            <td>{item.email}</td>
-          </tr>
-        ))}
-      </tbody>
+      <div className="container">
+
+        {/* <tbody className="list">
+          {data.map((item) => (
+            <tr key={item.id} className="listItem">
+              <td>{item.name}</td>
+              <td>{item.role}</td>
+              <td>{item.email}</td>
+            </tr>
+          ))}
+        </tbody> */}
+
+        <ul className="list">
+          {data.map((item) => (
+            <li key={item.id} className="listItem">
+              <p>{item.name}</p>
+              <p>{item.role}</p>
+              <p>{item.email}</p>
+            </li>
+          ))}
+        </ul>
+
+      </div>
     </table>
   )
 }
