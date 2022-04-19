c78a27889674cebdece659003ae60ba7ac749791
Olya <ola.shy@yahoo.com>
2022-04-19 13:50:43 +0200

    fix

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Table.css';
 import { Avatar } from '@mui/material';
 
 function Table({ data }) {
+  const [seed, setSeed] = useState('');
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 500));
+  }, []);
+
   return (
     <div className='wrapper_table'>
       <h1 className='title'>Selected Files</h1>
         <ul className="list">
           {data.map((item) => (
             <li key={item.id} className="listItem">
-              <Avatar className='icon' src='./assets/3.svg' />
+              <Avatar className='icon' src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`} />
               <p>{item.name}</p>
-              <p>{item.role}</p>
+              <p className='role' >{item.role}</p>
               <p>{item.email}</p>
             </li>
           ))}
