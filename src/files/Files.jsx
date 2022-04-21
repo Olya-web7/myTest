9b9370e36ccb114b9d02dfed51e372b3317d482f
Olya <ola.shy@yahoo.com>
2022-04-21 19:40:36 +0200

    fix

--- src/files/Files.jsx
+++ src/files/Files.jsx
@@ -13,10 +13,10 @@ function Files({ data }) {
           {data.map((item) => (
             <li key={item.id} className="listItem_files">
               <div className='file_name'>
-                <InsertDriveFileOutlinedIcon sx={{ color: item.color }} className='icon'/>
+                <InsertDriveFileOutlinedIcon sx={{ color: item.color }} className='icon_files'/>
                 <p>{item.name}</p>
               </div>
-              <Checkbox sx={{color: grey[600], '&.Mui-checked': {color: orange[600]}}}/>
+              <Checkbox sx={{color: grey[700], '&.Mui-checked': {color: orange[800]}}}/>
             </li>
           ))}
         </ul>
