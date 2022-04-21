7d8aeea53a763db5c1739be37f538f2aae385b6e
Olya <ola.shy@yahoo.com>
2022-04-21 20:36:05 +0200

    fix

--- src/table/MyTable.jsx
+++ src/table/MyTable.jsx
@@ -7,9 +7,42 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import { grey, orange } from '@mui/material/colors';
 
 export default function MyTable({ data }) {
+
+  const [checked, setChecked] = React.useState([true, false]);
+
+  const handleChange1 = (event) => {
+    setChecked([event.target.checked, event.target.checked]);
+  };
+
+  const handleChange2 = (event) => {
+    setChecked([event.target.checked, checked[1]]);
+  };
+
+  const handleChange3 = (event) => {
+    setChecked([checked[0], event.target.checked]);
+  };
+
+
   return (
-    <div className='wrapper_table'>
-      <h1 className='title'>Selected Files</h1>
+    <div className='wrapper_table'>      
+
+      <div className='table_header'>
+        <h1 className='title'>Selected Files</h1>
+        <div>
+          <span className='title'>Select all users</span>
+          <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={checked[0] && checked[1]}
+                      indeterminate={checked[0] !== checked[1]}
+                      onChange={handleChange1}
+                      sx={{color: grey[700], '&.Mui-checked': {color: orange[800]}}}
+                    />
+                  }
+                />
+        </div>
+      </div>
+
         <ul className="list">
           {data.map((item) => (
             <li key={item.id} className="listItem">
@@ -22,7 +55,9 @@ export default function MyTable({ data }) {
                 <div className='name_avatar'><p className={`role ${item.role}`} >{item.role}</p></div>
                 <p>{item.email}</p>
               </div>
-              <Checkbox sx={{color: grey[700], '&.Mui-checked': {color: orange[800]}}}/>
+              <Checkbox sx={{color: grey[700], '&.Mui-checked': {color: orange[800]}}}
+                checked={checked} onChange={handleChange1}
+                />
             </li>
           ))}
         </ul>
